1f25824e6a2be8e107918a88be36b2613151d944
luqiangbo <luqiangbo@orionstar.com>
2022-03-30 11:44:48 +0800

    ✨ react18

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from '@/App'
 import 'antd/dist/antd.less'
@@ -7,4 +7,5 @@ import '@/styles/index.less'
 import 'virtual:windi.css'
 import 'virtual:svg-icons-register'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
