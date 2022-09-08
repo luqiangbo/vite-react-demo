5322dbdd9fee4130bc4ff53b44979887fb906f11
luqiangbo <luqiangbo@orionstar.com>
2022-09-08 19:21:03 +0800

    🎨 升级菜单

--- src/pages/layout/menu.jsx
+++ src/pages/layout/menu.jsx
@@ -1,68 +1,38 @@
 import { useState, lazy } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Menu } from 'antd'
-import { HomeOutlined, ApartmentOutlined, FundOutlined } from '@ant-design/icons'
-import { has } from 'lodash-es'
+import { PieChartOutlined, DesktopOutlined, ContainerOutlined, MailOutlined, AppstoreOutlined } from '@ant-design/icons'
 
-const { SubMenu } = Menu
-
-const routerList = [
-  {
-    key: '/',
-    name: '首页',
-    icon: <HomeOutlined />,
-  },
-  {
-    key: '@limit',
-    name: '管理',
-    icon: <ApartmentOutlined />,
-    child: [
-      {
-        key: '/limit',
-        name: 'limit',
-        icon: <FundOutlined />,
-      },
-    ],
-  },
+const getItem = (label, key, icon, children, type) => {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+    onClick() {
+      console.log('点击', { key, children, label, type })
+    },
+  }
+}
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('Option 3', '3', <ContainerOutlined />),
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Option 7', '7'),
+    getItem('Option 8', '8'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+  ]),
 ]
-
 function Index() {
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const goto = (path) => {
-    navigate(path)
-  }
-
-  const dpList = (arr) => {
-    return arr.map((u) => {
-      if (has(u, 'child')) {
-        return (
-          <SubMenu key={u.key} icon={u.icon} title={u.name}>
-            {dpList(u.child)}
-          </SubMenu>
-        )
-      } else {
-        return (
-          <Menu.Item
-            key={u.key}
-            icon={u.icon}
-            onClick={() => {
-              goto(u.key)
-            }}
-          >
-            {u.name}
-          </Menu.Item>
-        )
-      }
-    })
-  }
-
-  return (
-    <Menu theme='dark' mode='inline' defaultSelectedKeys={[location.pathname]}>
-      {dpList(routerList)}
-    </Menu>
-  )
+  return <Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode='inline' theme='dark' items={items} />
 }
 
 export default Index
