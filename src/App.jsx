1f3d477cb2729fdee8ace3763a21559384f3143d
luqiangbo <luqiangbo@orionstar.com>
2022-11-18 17:41:33 +0800

    ✨ dayjs本地化

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
+import dayjs from 'dayjs'
 import zh_CN from 'antd/es/locale/zh_CN'
 import en_US from 'antd/es/locale/en_US'
+
+import 'dayjs/locale/zh-cn'
 
 import PageRoutes from '@c/PageRoutes'
 import Auth from '@c/Auth'
 import { useStore } from '@/store'
 
+dayjs.locale('zh-cn')
+
 function App() {
   const isAuth = useStore((state) => state.isAuth)
+
   console.log('app')
   return (
     <div>
