9bda4d1f6b8f701ab43291abf6862f9bb84b4675
luqiangbo <luqiangbo@orionstar.com>
2023-01-11 14:19:39 +0800

    🐛 样式调整

--- src/pages/layout/$index.jsx
+++ src/pages/layout/$index.jsx
@@ -1,28 +1,26 @@
 import { Outlet } from 'react-router-dom'
 import { useSetState } from 'ahooks'
-import { Layout, Col, Row } from 'antd'
+import { Layout, Row, Col } from 'antd'
 
 import CMenu from './menu'
 import './index.less'
 
-const { Content, Sider } = Layout
+const { Sider } = Layout
 
 function Index() {
-  const [state, setState] = useSetState({
-    collapsed: true,
-    width: '200px',
-  })
-
   return (
-    <Row className='layout-row'>
-      <Col className='layout-left'>
+    <div>
+      <Sider className='layout-sider'>
         <div className='logo'>LOGO</div>
         <CMenu />
-      </Col>
-      <Col className='layout-right'>
-        <Outlet />
-      </Col>
-    </Row>
+      </Sider>
+      <Row>
+        <Col className='layout-hide' />
+        <Col className='layout-content'>
+          <Outlet />
+        </Col>
+      </Row>
+    </div>
   )
 }
 
